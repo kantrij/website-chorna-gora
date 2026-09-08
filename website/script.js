8d4e46d774b3fcc03837757ac2cfab4306fdea09
kantrij <206023670+kantrij@users.noreply.github.com>
2026-09-08 21:45:48 +0300

    add js galere(not working yet) tryna to fix

---
+++ website/script.js
@@ -0,0 +1,69 @@
+const photos = document.querySelectorAll(".gallery img");
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImage = document.getElementById("lightboxImage");
+
+const closeButton = document.querySelector(".lightbox-close");
+const prevButton = document.querySelector(".lightbox-prev");
+const nextButton = document.querySelector(".lightbox-next");
+
+const counter = document.getElementById("lightbox-counter");
+
+let currentIndex = 0;
+
+function openGallery(index) {
+    currentIndex = index;
+
+    lightboxImage.src = photos[currentIndex].src;
+    lightboxImage.alt = photos[currentIndex].alt;
+
+    counter.textContent = `${currentIndex + 1} / ${photos.length}`;
+
+    lightbox.classList.add("active");
+}
+
+photos.forEach((photo, index) => {
+    photo.addEventListener("click", () => {
+        openGallery(index);
+    });
+});
+
+closeButton.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
+
+nextButton.addEventListener("click", () => {
+    currentIndex++;
+
+    if (currentIndex >= photos.length) {
+        currentIndex = 0;
+    }
+
+    openGallery(currentIndex);
+});
+
+prevButton.addEventListener("click", () => {
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = photos.length - 1;
+    }
+
+    openGallery(currentIndex);
+});
+
+document.addEventListener("keydown", (event) => {
+    if (!lightbox.classList.contains("active")) return;
+
+    if (event.key === "Escape") {
+        lightbox.classList.remove("active");
+    }
+
+    if (event.key === "ArrowRight") {
+        nextButton.click();
+    }
+
+    if (event.key === "ArrowLeft") {
+        prevButton.click();
+    }
+});
